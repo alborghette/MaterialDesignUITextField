a0f3ea210323eef7ea12e040ec2479cfc7bd42d5
murilo.alborghette <murilo.alborghette@mstech.com.br>
2015-10-07 11:58:20 -0300

    Add class to email validation.

---
+++ Example/MaterialDesignUITextField/NSString+EmailValidation.h
@@ -0,0 +1,13 @@
+//
+//  NSString+EmailValidation.h
+//  MaterialDesignUITextField
+//
+//  Created by Murilo Alborghette on 07/10/15.
+//  Copyright © 2015 murilo.alborghette. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (emailValidation)
+-(BOOL)isValidEmail;
+@end
---
+++ Example/MaterialDesignUITextField/NSString+EmailValidation.m
@@ -0,0 +1,22 @@
+//
+//  NSString+EmailValidation.m
+//  MaterialDesignUITextField
+//
+//  Created by Murilo Alborghette on 07/10/15.
+//  Copyright © 2015 murilo.alborghette. All rights reserved.
+//
+
+#import "NSString+EmailValidation.h"
+
+@implementation NSString (emailValidation)
+
+-(BOOL)isValidEmail {
+    BOOL stricterFilter = NO; // Discussion http://blog.logichigh.com/2010/09/02/validating-an-e-mail-address/
+    NSString *stricterFilterString = @"^[A-Z0-9a-z\\._%+-]+@([A-Za-z0-9-]+\\.)+[A-Za-z]{2,4}$";
+    NSString *laxString = @"^.+@([A-Za-z0-9-]+\\.)+[A-Za-z]{2}[A-Za-z]*$";
+    NSString *emailRegex = stricterFilter ? stricterFilterString : laxString;
+    NSPredicate *emailTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", emailRegex];
+    return [emailTest evaluateWithObject:self];
+}
+
+@end
